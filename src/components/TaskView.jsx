9453f280e888d68058c9922090148bc57ccc36df
cbakcoleman <88118758+cbakcoleman@users.noreply.github.com>
2021-11-01 20:17:38 -0700

    begin checkbox

--- src/components/TaskView.jsx
+++ src/components/TaskView.jsx
@@ -1,6 +1,16 @@
 import React from 'react'
+import {useState} from 'react'
+
 // COMPONENT TO VIEW LIST OF TASKS
 const TaskView = (props) => {
+    // VAR TO SET CHECKED
+    const [checked, setChecked] = useState(false);
+
+    // FUNCTION TO CHANGE CHECKED
+    const changeChecked = () => {
+        setChecked(!checked);
+    }
+
 // USE MAP TO DISPLAY ALL ITEMS IN TASKLIST
     return (
         <div>
@@ -8,11 +18,25 @@ const TaskView = (props) => {
             <h4>Your tasks:</h4>
             {
                 props.taskList.map((task, idx) => {
-                    return <li key={idx}>{task}</li>
+                    return (
+                        <li key={idx}>
+                            {task}
+                            <input type="checkbox" 
+                            checked={checked}
+                            onChange={changeChecked}/>
+                        </li>
+                    )
                 })
             }
+            <p>Is {props.taskList.task} checked? {checked.toString()}</p>
         </div>
     )
 }
 
 export default TaskView
+
+// *************WORKING NOTES****************
+// ADDED LINES 6-12
+// ADDED LINES 24-26
+// ADDED LINES 31-32
+// SEE ROBINEWIERUCK.DE/REACT-CHECKBOX ???
